43b49232e410f191c06bbc124871eb0322bcc8c0
Amir <106912785+am1r0d@users.noreply.github.com>
2023-10-16 04:48:50 +0330

    add Link to RecipePage to Cuisine and Searched - update App.js

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 // import { motion } from "framer-motion";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Cuisine = () => {
     //
@@ -26,8 +27,10 @@ const Cuisine = () => {
             {cuisine.map((item) => {
                 return (
                     <Card key={item.id}>
-                        <img src={item.image} alt={item.title} />
-                        <h4>{item.title}</h4>
+                        <Link to={"/recipe/" + item.id}>
+                            <img src={item.image} alt={item.title} />
+                            <h4>{item.title}</h4>
+                        </Link>
                     </Card>
                 );
             })}
